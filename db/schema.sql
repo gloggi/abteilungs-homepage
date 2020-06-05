994f225a3821aed06e3aaf2055cde108808f9fd5
Carlo Beltrame <carlo@beltra.me>
2020-06-06 00:04:21 +0200

    Add migration tool and automate directus database setup with it

---
+++ db/schema.sql
@@ -0,0 +1,369 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `directus_activity`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_activity` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `action` varchar(45) NOT NULL,
+  `action_by` int(11) unsigned NOT NULL DEFAULT 0,
+  `action_on` datetime NOT NULL,
+  `ip` varchar(50) NOT NULL,
+  `user_agent` varchar(255) NOT NULL,
+  `collection` varchar(64) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `edited_on` datetime DEFAULT NULL,
+  `comment` text CHARACTER SET utf8mb4 DEFAULT NULL,
+  `comment_deleted_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_collection_presets`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_collection_presets` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `user` int(11) unsigned DEFAULT NULL,
+  `role` int(11) unsigned DEFAULT NULL,
+  `collection` varchar(64) NOT NULL,
+  `search_query` varchar(100) DEFAULT NULL,
+  `filters` text DEFAULT NULL,
+  `view_type` varchar(100) NOT NULL DEFAULT 'tabular',
+  `view_query` text DEFAULT NULL,
+  `view_options` text DEFAULT NULL,
+  `translation` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_user_collection_title` (`user`,`collection`,`title`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_collections`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_collections` (
+  `collection` varchar(64) NOT NULL,
+  `managed` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  `hidden` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `single` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `icon` varchar(30) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `translation` text DEFAULT NULL,
+  PRIMARY KEY (`collection`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_fields`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_fields` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `collection` varchar(64) NOT NULL,
+  `field` varchar(64) NOT NULL,
+  `type` varchar(64) NOT NULL,
+  `interface` varchar(64) DEFAULT NULL,
+  `options` text DEFAULT NULL,
+  `locked` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `validation` varchar(500) DEFAULT NULL,
+  `required` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `readonly` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `hidden_detail` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `hidden_browse` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `sort` int(11) unsigned DEFAULT NULL,
+  `width` varchar(50) DEFAULT 'full',
+  `group` int(11) unsigned DEFAULT NULL,
+  `note` varchar(1024) DEFAULT NULL,
+  `translation` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_collection_field` (`collection`,`field`)
+) ENGINE=InnoDB AUTO_INCREMENT=169 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_files`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_files` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `storage` varchar(50) NOT NULL DEFAULT 'local',
+  `private_hash` varchar(16) DEFAULT NULL,
+  `filename_disk` varchar(255) NOT NULL,
+  `filename_download` varchar(255) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `uploaded_by` int(11) unsigned NOT NULL,
+  `uploaded_on` datetime NOT NULL,
+  `charset` varchar(50) DEFAULT NULL,
+  `filesize` int(11) unsigned NOT NULL DEFAULT 0,
+  `width` int(11) unsigned DEFAULT NULL,
+  `height` int(11) unsigned DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `embed` varchar(200) DEFAULT NULL,
+  `folder` int(11) unsigned DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `location` varchar(200) DEFAULT NULL,
+  `tags` varchar(255) DEFAULT NULL,
+  `checksum` varchar(32) DEFAULT NULL,
+  `metadata` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_folders`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_folders` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) CHARACTER SET utf8mb4 NOT NULL,
+  `parent_folder` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_name_parent_folder` (`name`,`parent_folder`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_migrations`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_migrations` (
+  `version` bigint(20) NOT NULL,
+  `migration_name` varchar(100) DEFAULT NULL,
+  `start_time` timestamp NULL DEFAULT NULL,
+  `end_time` timestamp NULL DEFAULT NULL,
+  `breakpoint` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_permissions`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_permissions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `collection` varchar(64) NOT NULL,
+  `role` int(11) unsigned NOT NULL,
+  `status` varchar(64) DEFAULT NULL,
+  `create` varchar(16) DEFAULT 'none',
+  `read` varchar(16) DEFAULT 'none',
+  `update` varchar(16) DEFAULT 'none',
+  `delete` varchar(16) DEFAULT 'none',
+  `comment` varchar(8) DEFAULT 'none',
+  `explain` varchar(8) DEFAULT 'none',
+  `read_field_blacklist` varchar(1000) DEFAULT NULL,
+  `write_field_blacklist` varchar(1000) DEFAULT NULL,
+  `status_blacklist` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_relations`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_relations` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `collection_many` varchar(64) NOT NULL,
+  `field_many` varchar(45) NOT NULL,
+  `collection_one` varchar(64) DEFAULT NULL,
+  `field_one` varchar(64) DEFAULT NULL,
+  `junction_field` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_revisions`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_revisions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `activity` int(11) unsigned NOT NULL,
+  `collection` varchar(64) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `data` longtext NOT NULL,
+  `delta` longtext DEFAULT NULL,
+  `parent_collection` varchar(64) DEFAULT NULL,
+  `parent_item` varchar(255) DEFAULT NULL,
+  `parent_changed` tinyint(1) unsigned DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_roles`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_roles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `ip_whitelist` text DEFAULT NULL,
+  `external_id` varchar(255) DEFAULT NULL,
+  `module_listing` text DEFAULT NULL,
+  `collection_listing` text DEFAULT NULL,
+  `enforce_2fa` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_group_name` (`name`),
+  UNIQUE KEY `idx_roles_external_id` (`external_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_settings`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_settings` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `key` varchar(64) NOT NULL,
+  `value` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_key` (`key`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_user_sessions`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_user_sessions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user` int(11) unsigned DEFAULT NULL,
+  `token_type` varchar(255) DEFAULT NULL,
+  `token` varchar(520) DEFAULT NULL,
+  `ip_address` varchar(255) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `token_expired_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_users`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(16) NOT NULL DEFAULT 'draft',
+  `role` int(11) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `timezone` varchar(32) NOT NULL DEFAULT 'America/New_York',
+  `locale` varchar(8) DEFAULT NULL,
+  `locale_options` text DEFAULT NULL,
+  `avatar` int(11) unsigned DEFAULT NULL,
+  `company` varchar(191) DEFAULT NULL,
+  `title` varchar(191) DEFAULT NULL,
+  `email_notifications` int(1) NOT NULL DEFAULT 1,
+  `last_access_on` datetime DEFAULT NULL,
+  `last_page` varchar(192) DEFAULT NULL,
+  `external_id` varchar(255) DEFAULT NULL,
+  `theme` varchar(100) DEFAULT 'auto',
+  `2fa_secret` varchar(100) DEFAULT NULL,
+  `password_reset_token` varchar(520) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_users_email` (`email`),
+  UNIQUE KEY `idx_users_token` (`token`),
+  UNIQUE KEY `idx_users_external_id` (`external_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directus_webhooks`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directus_webhooks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(16) NOT NULL DEFAULT 'inactive',
+  `http_action` varchar(255) DEFAULT NULL,
+  `url` varchar(510) DEFAULT NULL,
+  `collection` varchar(255) DEFAULT NULL,
+  `directus_action` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `schema_migrations`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'directus'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed
+
+--
+-- Dbmate schema migrations
+--
+
+LOCK TABLES `schema_migrations` WRITE;
+INSERT INTO `schema_migrations` (version) VALUES
+  ('20200606100000');
+UNLOCK TABLES;
