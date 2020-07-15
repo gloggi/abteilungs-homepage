e98d87fa68bc79e9e386139f7f80c164e89f9a09
Carlo Beltrame <carlo@beltra.me>
2020-07-15 12:02:49 +0200

    Display navbar on the bottom of pages with large banner
    Removes an unnecessary wrapping div (can be omitted due to v-if v-else)
    which broke the CSS rule for navbars following large headers.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,36 +1,34 @@
 <template>
-    <div>
-        <div v-if="$route.path=='/'" class="header-large">
-            <svg xmlns="http://www.w3.org/2000/svg" style="height:0; width:0; position: fixed;">
-                <defs>
-                    <filter id="shadow">
-                        <feDropShadow stdDeviation="4"/>
-                    </filter>
-                </defs>
-            </svg>
-            <!-- Post Thumbnail -->
-            <img class="header__image-large" :src="page.banner.data.full_url">
-            <div class="header__banner-large">
-                <img class="header__logo-large" :src="settings[0].logo.data.full_url" alt=""
-                     style="filter:url(#shadow)"/>
-                <h1 class="header__title">{{settings[0].scout_group_name}}</h1>
-            </div>
+    <div v-if="$route.path=='/'" class="header-large">
+        <svg xmlns="http://www.w3.org/2000/svg" style="height:0; width:0; position: fixed;">
+            <defs>
+                <filter id="shadow">
+                    <feDropShadow stdDeviation="4"/>
+                </filter>
+            </defs>
+        </svg>
+        <!-- Post Thumbnail -->
+        <img class="header__image-large" :src="page.banner.data.full_url">
+        <div class="header__banner-large">
+            <img class="header__logo-large" :src="settings[0].logo.data.full_url" alt=""
+                 style="filter:url(#shadow)"/>
+            <h1 class="header__title">{{settings[0].scout_group_name}}</h1>
         </div>
-        <div class="header" v-else>
-            <svg xmlns="http://www.w3.org/2000/svg" style="height:0; width:0; position: fixed;">
-                <defs>
-                    <filter id="shadow">
-                        <feDropShadow stdDeviation="4"/>
-                    </filter>
-                </defs>
-            </svg>
-            <img class="header__image-large" :src="page.banner.data.full_url">
-            <div class="header__banner">
-                <img class="header__logo" :src="settings[0].logo.data.full_url" alt="" style="filter:url(#shadow)"/>
-                <h1 class="header__title">{{settings[0].scout_group_name}}</h1>
-            </div>
-            <h1 class="header__heading">{{page.name}}</h1>
+    </div>
+    <div class="header" v-else>
+        <svg xmlns="http://www.w3.org/2000/svg" style="height:0; width:0; position: fixed;">
+            <defs>
+                <filter id="shadow">
+                    <feDropShadow stdDeviation="4"/>
+                </filter>
+            </defs>
+        </svg>
+        <img class="header__image-large" :src="page.banner.data.full_url">
+        <div class="header__banner">
+            <img class="header__logo" :src="settings[0].logo.data.full_url" alt="" style="filter:url(#shadow)"/>
+            <h1 class="header__title">{{settings[0].scout_group_name}}</h1>
         </div>
+        <h1 class="header__heading">{{page.name}}</h1>
     </div>
 </template>
 
