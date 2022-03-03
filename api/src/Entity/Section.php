917dd35d5cef92dc61b7c5a8195019f969a15bb1
tobifra <tobias.frauenfelder@gmail.com>
2022-03-03 21:01:28 +0100

    Add Sections

--- api/src/Entity/Section.php
+++ api/src/Entity/Section.php
@@ -5,49 +5,56 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\SectionRepository;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=SectionRepository::class)
+ * @ORM\Table(name="`section`")
  * @ApiResource(
  * collectionOperations={"get", "post"},
  *     itemOperations={
  *          "get"={},
- *          "put"
+ *          "put", "patch", "delete"
  *     },
- * normalizationContext={"groups"={"section:read"}}
+ * denormalizationContext={"groups"={"section:write"}},
+ * normalizationContext={"groups"={"section:read"}},
+ * 
  * )
  */
-#[ApiResource]
 class Section
 {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"group:read", "section:read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"group:read", "section:read"})
+     * @Groups({"group:read", "section:read", "section:write"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Groups({"group:read", "section:read", "section:write"})
      */
-    private $from_age;
+    private $fromAge;
 
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="integer", nullable=true)
+     * @Groups({"group:read", "section:read", "section:write"})
      */
-    private $to_age;
+    private $toAge;
 
     /**
      * @ORM\OneToMany(targetEntity=Group::class, mappedBy="section")
+     * @Groups({"group:read", "section:read"})
      */
     private $groups;
 
@@ -75,24 +82,24 @@ class Section
 
     public function getFromAge(): ?int
     {
-        return $this->from_age;
+        return $this->fromAge;
     }
 
-    public function setFromAge(?int $from_age): self
+    public function setFromAge(?int $fromAge): self
     {
-        $this->from_age = $from_age;
+        $this->fromAge = $fromAge;
 
         return $this;
     }
 
     public function getToAge(): ?int
     {
-        return $this->to_age;
+        return $this->toAge;
     }
 
-    public function setToAge(int $to_age): self
+    public function setToAge(?int $toAge): self
     {
-        $this->to_age = $to_age;
+        $this->toAge = $toAge;
 
         return $this;
     }
