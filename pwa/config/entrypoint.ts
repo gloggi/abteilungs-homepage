92e1b4b241173bf5e0b8bb860f682c77a5aa178d
Tobias Frauenfelder <tobias.frauenfelder@gmail.com>
2023-12-07 22:02:04 +0100

    Remove API-Platform from master branch

--- pwa/config/entrypoint.ts
+++
@@ -1,1 +0,0 @@
-export const NEXT_PUBLIC_ENTRYPOINT = typeof window === "undefined" ? process.env.NEXT_PUBLIC_ENTRYPOINT : window.origin;
--- pwa/next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  serverRuntimeConfig: {
-    NEXT_PUBLIC_ENTRYPOINT: process.env.NEXT_PUBLIC_ENTRYPOINT || "http://localhost",
-  },
-};
