4026b69514eb5f03e9ed7657a4ce7322c37a8a32
Tobias Frauenfelder <tobias.frauenfelder@gmail.com>
2023-12-09 18:53:18 +0100

    Update Tailwind configuration and dependencies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './styles/app.css';
 import {mixin} from './mixins.js'
 import './assets/tailwind.css'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,vue}"],
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require('@tailwindcss/forms')],
 }
+
