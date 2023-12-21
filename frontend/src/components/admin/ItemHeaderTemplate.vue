d7db280561f08a9d1d90e5f7658099e4393674af
Tobias Frauenfelder <tobias.frauenfelder@gmail.com>
2023-12-21 23:24:41 +0100

    Implement event dublication. Closes #4

--- frontend/src/components/admin/ItemHeaderTemplate.vue
+++ frontend/src/components/admin/ItemHeaderTemplate.vue
@@ -4,28 +4,31 @@
   </Card>
   <div class="flex justify-between mb-2">
     <router-link :to="{ name: backLinkTo }">
-      <button class="rounded-l-lg bg-white p-1">
-        <font-awesome-icon :icon="icons.faChevronLeft" class="h-6 w-6 text-gray-400" />
+      <button class="rounded-l-lg bg-white p-1 transition-colors duration-300 ease-in-out hover:bg-gray-400 text-gray-400 hover:text-white">
+        <font-awesome-icon :icon="icons.faChevronLeft" class="h-6 w-6 " />
       </button>
     </router-link>
     <div>
-      <button v-if="!noDelete" @click="deleteItem" class="bg-white p-1 border-r border-l border-gray-200">
-        <font-awesome-icon :icon="icons.faTrash" class="h-6 w-6 text-gray-400" />
+      <button v-if="dublicate" @click="dublicateItem" class="bg-white p-1 border-r border-l border-gray-200 transition-colors duration-300 ease-in-out hover:bg-gray-400 text-gray-400 hover:text-white">
+        <font-awesome-icon :icon="icons.faCopy" class="h-6 w-6" />
       </button>
-      <button class="rounded-r-lg bg-white p-1" @click="updateItem">
-        <font-awesome-icon :icon="icons.faArrowsRotate" class="h-6 w-6 text-gray-400" />
+      <button v-if="!noDelete" @click="deleteItem" class="bg-white p-1 border-r border-l border-gray-200 transition-colors duration-300 ease-in-out hover:bg-gray-400 text-gray-400 hover:text-white">
+        <font-awesome-icon :icon="icons.faTrash" class="h-6 w-6" />
+      </button>
+      <button class="rounded-r-lg bg-white p-1 transition-colors duration-300 ease-in-out hover:bg-gray-400 text-gray-400 hover:text-white" @click="updateItem">
+        <font-awesome-icon :icon="icons.faArrowsRotate" class="h-6 w-6 " />
       </button>
     </div>
   </div>
 </template>
 <script>
 import Card from "../../components/admin/Card.vue";
-import { faArrowsRotate, faChevronLeft, faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate, faChevronLeft, faTrash, faPlus, faCopy } from "@fortawesome/free-solid-svg-icons";
 export default {
   components: {
     Card,
   },
-  props: ["title", "content", "backLinkTo", "entity", "noDelete"],
+  props: ["title", "content", "backLinkTo", "entity", "noDelete", "dublicate"],
   emits: ["errors", "clearErrors"],
   data() {
     return {
@@ -34,6 +37,7 @@ export default {
         faChevronLeft,
         faTrash,
         faPlus,
+        faCopy
       },
       isDragging: false,
     };
@@ -60,6 +64,23 @@ export default {
           this.$router.push({ name: this.$route.name, params: { id: response.data.id } });
         }
         this.notifyUser("Item created");
+      } catch (e) {
+        if(e.response.status === 422){
+          this.$emit("errors", e.response.data.errors)
+        }
+      }
+    },
+    async dublicateItem() {
+      try {
+        const copy = {...this.content};
+        delete copy.id;
+        delete copy.createdAt;
+        delete copy.updatedAt;
+        const response = await this.callApi("post", `/${this.entity}`, copy);
+        if(response.data.id){
+          this.$router.push({ name: this.$route.name, params: { id: response.data.id } });
+        }
+        this.notifyUser("Item dublicated");
       } catch (e) {
         if(e.response.status === 422){
           this.$emit("errors", e.response.data.errors)
