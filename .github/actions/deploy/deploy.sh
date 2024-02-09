bbcffe1edfad66ca5de1e9aeeb78053eab1bb463
carlobeltrame <carlo@beltra.me>
2024-02-09 18:33:47 +0100

    Add deployment workflow

---
+++ .github/actions/deploy/deploy.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+set -euo pipefail
+
+
+### BACKEND PREPARATION
+
+cd api
+
+rm -f .env
+cp .env.example .env
+
+if [ "$APP_NAME" != "" ]; then
+  sed -ri "s~^#APP_NAME=.*$~APP_NAME=$APP_NAME~" .env
+fi
+
+sed -ri "s~^APP_ENV=.*$~APP_ENV=$APP_ENV~" .env
+sed -ri "s~^APP_KEY=.*$~APP_KEY=$APP_KEY~" .env
+sed -ri "s~^APP_DEBUG=.*$~APP_DEBUG=$APP_DEBUG~" .env
+sed -ri "s~^APP_URL=.*$~APP_URL=$API_URL~" .env
+
+sed -ri "s~^DB_HOST=.*$~DB_HOST=$DB_HOST~" .env
+sed -ri "s~^DB_DATABASE=.*$~DB_DATABASE=$DB_DATABASE~" .env
+sed -ri "s~^DB_USERNAME=.*$~DB_USERNAME=$DB_USERNAME~" .env
+sed -ri "s~^DB_PASSWORD=.*$~DB_PASSWORD=$DB_PASSWORD~" .env
+
+sed -ri "s~^SESSION_SECURE_COOKIE=.*$~SESSION_SECURE_COOKIE=true~" .env
+
+sed -ri "s~^MIDATA_BASE_URL=.*$~MIDATA_BASE_URL=$MIDATA_BASE_URL~" .env
+sed -ri "s~^MIDATA_CLIENT_UID=.*$~MIDATA_CLIENT_UID=$MIDATA_CLIENT_UID~" .env
+sed -ri "s~^MIDATA_CLIENT_SECRET=.*$~MIDATA_CLIENT_SECRET=$MIDATA_CLIENT_SECRET~" .env
+sed -ri "s~^MIDATA_CALLBACK_URI=.*$~MIDATA_CALLBACK_URI=${API_URL}/login/hitobito/callback~" .env
+
+docker compose run --no-deps --entrypoint "composer install --no-dev" backend
+
+PHP_MIN_VERSION_ID=$(grep -Po '(?<=\(PHP_VERSION_ID >= )[0-9]+(?=\))' vendor/composer/platform_check.php)
+
+cd ..
+
+
+### FRONTEND PREPARATION
+
+cd frontend
+
+rm -f .env
+cp .env.example .env
+
+sed -ri "s~^VITE_BACKEND_URL=.*$~VITE_BACKEND_URL=$API_URL~" .env
+
+docker compose run --rm --no-deps --entrypoint "/bin/sh -c 'npm install && npm run build --no-unsafe-inline'" frontend
+
+cd ..
+
+
+### DEPLOY ALL FILES
+
+echo "Scanning ssh host keys of \"$SSH_HOST\" (showing hashed output only):"
+ssh-keyscan -H $SSH_HOST
+
+echo "Showing configured know_hosts:"
+cat ~/.ssh/known_hosts
+
+echo "Checking PHP version:"
+ssh -l $SSH_USERNAME -T $SSH_HOST <<EOF
+  set -e
+  php -v
+  cd $SSH_BACKEND_DIRECTORY
+  php -r "if(PHP_VERSION_ID<${PHP_MIN_VERSION_ID:-80100}){echo \"Your PHP version is too old\\nYou might be able to use these instructions on your hosting as well: https://www.cyon.ch/support/a/php-standardversion-fur-die-kommandozeile-festlegen\n\";exit(1);}"
+
+  if [ -d "api" ]; then
+    cd api
+    php artisan down --render=updating
+  fi
+EOF
+
+echo "Uploading backend files to the server..."
+cd api
+lftp <<EOF
+  set sftp:auto-confirm true
+  set dns:order "inet"
+  open -u $SSH_USERNAME, sftp://$SSH_HOST
+  cd $SSH_BACKEND_DIRECTORY
+  mirror -enRv -x '^\.' -x '^storage/logs/.*' -x '^storage/app/.*' -x '^storage/framework/maintenance.php$' -x '^storage/framework/down$' -x '^resources/fonts/.*' -x '^resources/images/.*' -x '^resources/js/.*' -x '^resources/css/.*'
+  mirror -Rv -f .env
+EOF
+cd ..
+echo "All backend files uploaded to the server."
+
+echo "Uploading frontend files to the server..."
+cd frontend
+cd frontend/dist
+lftp <<EOF
+  set sftp:auto-confirm true
+  set dns:order "inet"
+  open -u $SSH_USERNAME, sftp://$SSH_HOST
+  cd $SSH_FRONTEND_DIRECTORY
+  mirror -enRv -x '^\.'
+EOF
+cd ..
+echo "All frontend files uploaded to the server."
+
+ssh -l $SSH_USERNAME -T $SSH_HOST <<EOF
+  cd $SSH_BACKEND_DIRECTORY
+  php artisan storage:link
+  php artisan migrate --force
+
+  php artisan config:cache
+  php artisan route:cache
+  php artisan view:cache
+
+  php artisan up
+EOF
---
+++ api/resources/views/updating.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Update</title>
+
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Source Sans Pro', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .code {
+            border-right: 1px solid #a7abad;
+            font-size: 26px;
+            padding: 0 15px 0 15px;
+            text-align: center;
+        }
+
+        .message {
+            font-size: 18px;
+            text-align: center;
+            padding: 10px;
+            color: #2c3135;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="code">503</div>
+    <div class="message">
+        <p>
+        Diese Seite wird gerade aktualisiert. Bitte einen Moment Geduld.
+        </p>
+    </div>
+</div>
+</body>
+</html>
