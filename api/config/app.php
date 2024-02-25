68defe35e3bfca75e647e467564f891c1f3a2bb3
Tobias Frauenfelder <tobias.frauenfelder@gmail.com>
2024-02-25 19:49:50 +0100

    Add frontend URL configuration

--- api/config/app.php
+++ api/config/app.php
@@ -58,6 +58,8 @@ return [
 
     'asset_url' => env('ASSET_URL'),
 
+    'frontend_url' => env('FRONTEND_URL'),
+    
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
--- api/routes/web.php
+++ api/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function() {
-    return redirect(env('FRONTEND_URL'));
+    return redirect(config('app.frontend_url'));
 });
