953fc871cd388789b83a49c18ebd6bec6a3c96fe
Tobias Frauenfelder <tobias.frauenfelder@gmail.com>
2024-03-20 21:47:38 +0100

    Make MultipleSelect Scrollable

--- frontend/src/components/admin/MultipleSelect.vue
+++ frontend/src/components/admin/MultipleSelect.vue
@@ -20,7 +20,7 @@
           }}
         </option>
       </select>
-      <div v-if="showDropdown" class="absolute bg-white z-30 rounded-lg w-full">
+      <div v-if="showDropdown" class="absolute bg-white z-30 rounded-lg w-full overflow-auto max-h-60">
         <div
           v-for="option in options"
           :key="option.id"
