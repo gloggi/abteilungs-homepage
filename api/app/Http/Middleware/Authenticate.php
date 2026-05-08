397a89c9263640c59c57ef0f0df85f6b3e09234a
Tobias Frauenfelder <tobifra@users.noreply.github.com>
2026-05-08 17:25:37 +0200

    Fix wrong route redirection

--- api/app/Http/Middleware/Authenticate.php
+++ api/app/Http/Middleware/Authenticate.php
@@ -12,6 +12,10 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (! $request->expectsJson()) {
+            abort(403, 'Access denied. You must be logged in.');
+        }
+
+        return null;
     }
 }
